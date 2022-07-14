6ce6967756efbcc1987b0a8c69b1f36db2055d46
caiocipriano <caio.silvax@hotmail.com>
2022-07-14 10:41:24 -0300

    feat: exception error

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,8 @@
-import  express  from "express";
+import  express, { NextFunction, Request, Response }  from "express";
 import cors from 'cors'
 
-
 import routes from './routes/index'
+import AppError from "@shared/errors/AppError";
 
 const app = express()
 
@@ -10,6 +10,19 @@ app.use(cors())
 app.use(express.json())
 
 app.use(routes)
+
+app.use((erro:Error, request:Request,response:Response, next:NextFunction)=>{
+    if(erro instanceof AppError){
+        return response.status(erro.statusCode).json({
+            status:'Error',
+            message:erro.message,
+        })
+    }
+    return response.status(500).json({
+        status:'Error',
+        message:'Internal Server Error'
+    })
+})
 
 app.listen(8080,()=>{
     console.log("Server running!🤖")
