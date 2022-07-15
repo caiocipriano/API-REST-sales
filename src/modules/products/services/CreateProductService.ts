357d3e875f8835d7fc522a1a4174353f01e2da79
caiocipriano <caio.silvax@hotmail.com>
2022-07-15 14:06:57 -0300

    feat:CreateProductService

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,34 @@
+import { ProductRepository } from './../typeorm/repositories/ProductRepository';
+import { getCustomRepository } from 'typeorm';
+import { Product } from './../typeorm/entities/Product';
+import AppError from '@shared/errors/AppError';
+interface IRequest{
+    name:string,
+    quantity:number,
+    price:number
+}
+
+
+class CreateProductsService{
+    public async execute({name,quantity,price}:IRequest):Promise<Product>{
+        const productRepository = getCustomRepository(ProductRepository);
+        const productExist= await productRepository.findByName(name);
+
+        if(productExist){
+            throw new AppError("Product Exist")
+        }
+
+        const product = productRepository.create({
+            name,
+            quantity,
+            price
+        })
+
+        await productRepository.save(product)
+
+        return product
+
+    }
+}
+
+export default CreateProductsService
