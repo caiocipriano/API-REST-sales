5c6cd834d5b02db62c5ba0a7c317fa2f2cfee3c4
caiocipriano <caio.silvax@hotmail.com>
2022-07-15 14:12:38 -0300

    feat:ListProductService

---
+++ src/modules/products/services/ListProductService.ts
@@ -0,0 +1,15 @@
+import { ProductRepository } from './../typeorm/repositories/ProductRepository';
+import { getCustomRepository } from 'typeorm';
+import { Product } from './../typeorm/entities/Product';
+
+class ListProductService{
+    public async execute(): Promise<Product[]>{
+        const productRepository = getCustomRepository(ProductRepository);
+
+        const products = productRepository.find()
+
+        return products
+    }
+}
+
+export default ListProductService
