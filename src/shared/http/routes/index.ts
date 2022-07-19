a669287fa52eee57589c3718a7f01dcebd0fae42
caiocipriano <caio.silvax@hotmail.com>
2022-07-19 10:44:28 -0300

    feat: using user routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
 import productsRoutes from "@modules/products/routes/productsroutes";
+import userRoutes from "@modules/users/routes/userRoutes";
 import { Router } from "express";
 
 const routes = Router()
 
 routes.use("/products",productsRoutes)
+routes.use("/users",userRoutes)
 
 export default routes
