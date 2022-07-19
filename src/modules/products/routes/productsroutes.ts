49a5d55d2c56e89346214c6f5c3e5174b28b0c48
caiocipriano <caio.silvax@hotmail.com>
2022-07-19 10:06:46 -0300

    feat: adding celebrate middleware

--- src/modules/products/routes/productsroutes.ts
+++ src/modules/products/routes/productsroutes.ts
@@ -1,13 +1,40 @@
 import { Router } from "express";
 import ProductController from "../controller/ProductController";
+import {celebrate, Joi, Segments} from 'celebrate'
 
 const productsRoutes = Router();
 const productController = new ProductController();
 
 productsRoutes.get("/", productController.findAll);
-productsRoutes.get("/:id", productController.findById);
-productsRoutes.post("/", productController.create);
-productsRoutes.put("/:id", productController.update);
-productsRoutes.delete("/:id",productController.deleteById);
+
+productsRoutes.get("/:id", 
+    celebrate({
+        [Segments.PARAMS]:{
+            id:Joi.number().id().required()
+    }}),productController.findById);
+
+productsRoutes.post("/", 
+    celebrate({
+        [Segments.BODY]:{
+            name:Joi.string().required(),
+            price:Joi.number().precision(2).required(),
+            quantity:Joi.number().required()}
+    }),productController.create);
+
+productsRoutes.put("/:id",
+    celebrate({
+        [Segments.BODY]:{
+            name:Joi.string().required(),
+            price:Joi.number().precision(2).required(),
+            quantity:Joi.number().required()},
+        [Segments.PARAMS]:{
+            id:Joi.number().id().required()
+    }}),productController.update);
+
+productsRoutes.delete("/:id",
+    celebrate({
+        [Segments.PARAMS]:{
+        id:Joi.number().id().required()
+    }}),productController.deleteById);
 
 export default productsRoutes
